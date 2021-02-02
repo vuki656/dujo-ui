cdcc3952eeb19421a0a5e64e21ff023e68b0e8f8
vuki656 <off.vukovic@gmail.com>
2021-02-02 21:08:01 +0100

    feature(tooltip): add tippy prop types

--- src/Tooltip/Tooltip.types.ts
+++ src/Tooltip/Tooltip.types.ts
@@ -1,4 +1,6 @@
-export type TooltipProps = {
+import type { TippyProps } from '@tippyjs/react'
+
+export type TooltipProps = Omit<TippyProps, 'delay'> & {
     label: string
     enterDelay?: number
     exitDelay?: number
